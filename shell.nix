6e5855b1670dc0e28d199c79479fe3911af881da
Nikolay Puzanov <np@embddr.xyz>
2026-06-16 14:18:34 +0300

    docs: add implementation plan and dev environment
    - PLAN.md: 10-step plan (data structures, config parser, regulate module,
      GPU ops, main loop, CLI, Makefile, example/test configs, unit tests,
      manual testing)
    - shell.nix: nix shell with gnumake and gpu-burn

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+let gpu-burn-pkg = (
+      import <nixos> {
+        config.cudaSupport = true;
+        config.allowUnfree = true;
+      }).gpu-burn;
+in
+mkShell {
+  packages = [ gnumake gpu-burn-pkg ];
+
+  shellHook = ''
+    export NIX_SHELL_NAME="NVPG"
+  '';
+}
