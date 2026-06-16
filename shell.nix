913a7034d17f05c45234cd765b8bbfa0335c53b5
Nikolay Puzanov <np@embddr.xyz>
2026-06-16 17:54:37 +0300

    refactor: simplify shell.nix
    - remove gpu-burn dependency (imported via nixos with cudaSupport)
    - keep only gnumake and libc.static for development

--- shell.nix
+++ shell.nix
@@ -1,16 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
 
-with pkgs;
-let gpu-burn-pkg = (
-      import <nixos> {
-        config.cudaSupport = true;
-        config.allowUnfree = true;
-      }).gpu-burn;
-in
-mkShell {
-  packages = [ gnumake libc.static gpu-burn-pkg ];
-
-  shellHook = ''
-    export NIX_SHELL_NAME="NVPG"
-  '';
+with pkgs; mkShell {
+  packages = [ gnumake libc.static ];
 }
